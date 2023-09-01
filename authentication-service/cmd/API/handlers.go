240ac508897366cd5cad5cd3557e9483d610524d
GUSTAVO MITSUO FERNANDES VALENTE TAKEDA <gustavomitsuo@tre-ce.jus.br>
2023-09-01 14:33:15 -0300

    Finished the logging service and integration

--- authentication-service/cmd/API/handlers.go
+++ authentication-service/cmd/API/handlers.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -30,6 +33,13 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//Log authentication
+	err = app.logRequest("authentication", fmt.Sprintf("%s logged in", user.Email))
+	if err != nil{
+		log.Panic(err)
+		return 
+	}
+
 	payload := jsonResponse {
 		Error: false,
 		Message: fmt.Sprintf("Logged in user %s", user.Email),
@@ -38,3 +48,29 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request) {
 
 	app.writeJSON(w, http.StatusAccepted, payload)
 }
+
+func (app *Config) logRequest(name, data string) error {
+	var entry struct {
+		Name string `json:"name"`
+		Data string `json:"data"`
+	}
+
+	entry.Name = name
+	entry.Data = data
+
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+	logServiceURL := "http://logger-service/log"
+
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	_, err = client.Do(request)
+	if err != nil {
+		return err
+	}
+	
+	return nil
+} 
